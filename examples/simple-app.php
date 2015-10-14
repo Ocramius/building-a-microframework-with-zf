2bd5d27756ba8a343d183fd9cdb294b6c7965d5c
Marco Pivetta <ocramius@gmail.com>
2015-10-15 01:20:20 +0200

    Simple app design

---
+++ examples/simple-app.php
@@ -0,0 +1,56 @@
+<?php
+
+// Note: we assume that everyone got their player token already!
+
+use Poker\Game\Player;
+use Poker\Game;
+use Ramsey\Uuid\Uuid;
+
+(function () {
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    set_error_handler(function () {
+        http_response_code(500);
+
+        json_encode(['error' => 'something went wrong']);
+    });
+
+    $game        = $_GET['game_id'] ?? null;
+    $playerToken = $_GET['player'] ?? null;
+    $action      = $_GET['action'] ?? null;
+    $httpMethod  = strtoupper($_SERVER['REQUEST_METHOD']);
+
+    $getGame = function ($gameId) {
+
+    };
+
+    switch (strtoupper($_SERVER['REQUEST_METHOD'])) {
+        case 'GET':
+            switch ($action) {
+                case 'create-game':
+                    $gameId = Uuid::uuid4();
+
+                    // hardcoded
+                    $game = Game::fromPlayers();
+            }
+
+            break;
+        case 'POST':
+            switch ($action) {
+                case 'create-game':
+                    // hardcoded
+                    list($game, $playerTokens) = Game::fromPlayers(
+                        Player::fromCash(100),
+                        Player::fromCash(150),
+                        Player::fromCash(125),
+                        Player::fromCash(95)
+                    );
+
+                    file_put_contents(__DIR__ . '/data/poker-games/' . (string) Uuid::uuid4(), serialize($game));
+
+                    echo json_encode(['player-tokens' => array_map('strval', $playerTokens)]);
+            }
+
+            break;
+    }
+})();
